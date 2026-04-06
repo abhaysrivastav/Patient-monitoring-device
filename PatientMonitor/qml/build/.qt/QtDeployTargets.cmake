67d01fb36fce9e0a380cbc808b5d0a5333e54497
abhaysrivastav <abhay.ercomp@gmail.com>
2026-04-06 22:10:12 +0530

    Ignore .qtcreator and build folders

--- PatientMonitor/qml/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PatientMonitor_QML_FILE C:/Users/Admin/Documents/Claude/Projects/Freelauncing-1/PatientMonitor/qml/build/PatientMonitor_QML.exe)
-set(__QT_DEPLOY_TARGET_PatientMonitor_QML_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PatientMonitor_QML_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Quick.dll;C:/msys64/mingw64/bin/Qt6OpenGL.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6QmlMeta.dll;C:/msys64/mingw64/bin/Qt6QmlModels.dll;C:/msys64/mingw64/bin/Qt6QmlWorkerScript.dll;C:/msys64/mingw64/bin/Qt6Qml.dll;C:/msys64/mingw64/bin/Qt6Network.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
